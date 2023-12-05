1755e8caafb8312f2febf216d3d541c47bc30c4b
Christopher <114704469+Blade-24@users.noreply.github.com>
2023-12-05 07:59:05 -0600

    useReducer with switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import Counter from "./components/Counter.jsx";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount(count + 1);
-  const decrement = () => {
-    if (count === 0) return count;
-    return setCount(count - 1);
+function initialState() {
+  return {
+    count: 0,
   };
-  const reset = () => setCount(0);
+}
+
+function counterReducer(state = initialState(), action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      if (state.count === 0) return initialState();
+      return { count: state.count - 1 };
+    case "RESET":
+      return initialState();
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(counterReducer, initialState());
+
+  const increment = () => dispatch({ type: "INCREMENT" });
+  const decrement = () => dispatch({ type: "DECREMENT" });
+  const reset = () => dispatch({ type: "RESET" });
 
   return (
     <div className="h-screen w-screen bg-zinc-100 flex justify-center items-center flex-col gap-7">
@@ -17,7 +32,7 @@ function App() {
         <h1 className="text-2xl uppercase font-bold">Counter</h1>
       </header>
       <Counter
-        value={count}
+        value={state.count}
         add={increment}
         less={decrement}
         restart={reset}
